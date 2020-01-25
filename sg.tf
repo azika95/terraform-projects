c023b92779f804f77a155effea17db0dfa16fa1d
Anara KD <rodnaya2303@gmail.com>
2020-01-25 14:42:02 -0600

    added

--- sg.tf
+++ sg.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "allow_tls" {
     protocol    = "tcp"
     # Please restrict your ingress to only necessary IPs and ports.
     # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
-    cidr_blocks = # add a CIDR block here
+    cidr_blocks = ["0.0.0.0/0"] # add a CIDR block here
   }
 
   egress {
