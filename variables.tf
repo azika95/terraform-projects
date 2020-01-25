e8dc1198f4f195332e0d1a15e860146b1d2e5d02
Anara KD <rodnaya2303@gmail.com>
2020-01-25 15:04:57 -0600

    added

--- variables.tf
+++ variables.tf
@@ -1,6 +1,12 @@
-variable "bucket" {}
+variable "s3_bucket" {}
 
-variable "region" {}
+variable "s3_folder_project" {}
+ 
+variable "s3_bucket_region" {}
+
+variable "s3_tfstate_file" {}
+
+variable "s3_folder_type" {}
 
 variable "private_subnet_id_1" {}
 
@@ -9,3 +15,7 @@ variable "private_subnet_id_2" {}
 variable "private_subnet_id_3" {}
 
 variable "cluster_identifier" {}
+
+variable "s3_folder_name" {}
+
+
